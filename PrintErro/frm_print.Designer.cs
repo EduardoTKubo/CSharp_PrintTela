b3c6d0229056795869dca51fd119154a1b714821
ekubo <eduardo.tkb@gmail.com>
2019-10-04 19:55:37 -0300

    20191001 Print da Tela

---
+++ PrintErro/frm_print.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PrintErro
+{
+    partial class frm_print
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPrint = new System.Windows.Forms.Button();
+            this.lbObs = new System.Windows.Forms.Label();
+            this.tbObsv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btPrint
+            // 
+            this.btPrint.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPrint.Location = new System.Drawing.Point(0, 0);
+            this.btPrint.Name = "btPrint";
+            this.btPrint.Size = new System.Drawing.Size(318, 37);
+            this.btPrint.TabIndex = 0;
+            this.btPrint.Text = "Print Erro";
+            this.btPrint.UseVisualStyleBackColor = true;
+            this.btPrint.Click += new System.EventHandler(this.btPrint_Click);
+            // 
+            // lbObs
+            // 
+            this.lbObs.AutoSize = true;
+            this.lbObs.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbObs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbObs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbObs.Location = new System.Drawing.Point(12, 46);
+            this.lbObs.Name = "lbObs";
+            this.lbObs.Size = new System.Drawing.Size(33, 13);
+            this.lbObs.TabIndex = 1;
+            this.lbObs.Text = "Obs:";
+            this.lbObs.DoubleClick += new System.EventHandler(this.lbObs_DoubleClick);
+            // 
+            // tbObsv
+            // 
+            this.tbObsv.Location = new System.Drawing.Point(52, 43);
+            this.tbObsv.Name = "tbObsv";
+            this.tbObsv.Size = new System.Drawing.Size(254, 20);
+            this.tbObsv.TabIndex = 2;
+            this.tbObsv.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbObsv_KeyPress);
+            // 
+            // frm_print
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(318, 75);
+            this.Controls.Add(this.tbObsv);
+            this.Controls.Add(this.lbObs);
+            this.Controls.Add(this.btPrint);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frm_print";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Resize += new System.EventHandler(this.frm_print_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPrint;
+        private System.Windows.Forms.Label lbObs;
+        private System.Windows.Forms.TextBox tbObsv;
+    }
+}
+
---
+++ PrintErro/frm_print.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PrintErro
+{
+    public partial class frm_print : Form
+    {
+        public frm_print()
+        {
+            InitializeComponent();
+            this.Text = "Print de Tela";
+        }
+
+
+        private void btPrint_Click(object sender, EventArgs e)
+        {
+            PrintScreen();
+        }
+
+
+        private void PrintScreen()
+        {
+            string caminho = "";
+            string nomeimagem = "";
+
+            if (tbObsv.Text == "")
+            {
+                MessageBox.Show("Campo Obsv não pode ficar em branco!", "Print Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                this.Hide();
+                //System.Threading.Thread.Sleep(2000);
+                //SendKeys.Send("{PRTSC}");
+                
+                //Image img = Clipboard.GetImage();
+
+                Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+                Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+                graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+
+                caminho = @"\\srvfiles\Departamentos\Supervisao\Print_Erros\";
+
+
+                string Data = //DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() +"_" +
+                    DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString();
+
+                nomeimagem = "Erro_" + Data + "_"+ tbObsv.Text  +"_"+ Environment.MachineName + ".jpg";
+
+                printscreen.Save(caminho + nomeimagem);
+
+                this.Show();
+
+                MessageBox.Show("Imagem salva com sucesso!", "Print Screen", MessageBoxButtons.OK ,MessageBoxIcon.Information);
+
+                Application.Exit();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Verifique o nome digitado ele contem caractares invalidos!. Imagem não foi salva"
+                                + Environment.NewLine + e.Message, "Imagem não foi salva", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Show();
+            }
+        }
+
+
+        private void tbObsv_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                this.Close();
+            }  
+        }
+
+
+        private void lbObs_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"\\srvfiles\Departamentos\Supervisao\Print_Erros\", "Print gravado na pasta ...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void frm_print_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Maximized)
+            {
+                this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            }
+        }
+
+    }
+}
